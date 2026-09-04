76b0206f04e89924c7e83337f358bc66f111c2fd
Lennart Chua <lennartchua59@gmail.com>
2026-09-04 16:31:40 +0800

    comments

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+#Activity 1
 print("=================================")
 print("Welcome here")
 print("My first post!")
 print("=================================")
 
+
+#Activity 2
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -11,6 +14,8 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+
+#Activity 3
 followers += 50
 print("Day 1:", followers)
 
@@ -20,6 +25,7 @@ print("Day 2:", followers)
 followers -= 10
 print("Day 3:", followers)
 
+#Activity 4
 username = input("Enter Username: ")
 age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
@@ -30,5 +36,8 @@ print("Username:", username)
 print("Age:", age)
 print("Category:", category)
 
+
+
+#Activity 5
 if age > 40 and category == "fun":
     print("You are old what is fun for you??")
